db32a4f46289a12a84bf5442d3f988c4be0942d6
Brian Walsh <wal21017@byui.edu>
2026-01-08 20:50:14 -0700

    Prep Assignment 2 complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.WriteLine("Welcome to the AutoGrader!");
+        Console.Write("What grade percentage did you get in your course? ");
+        string grade = Console.ReadLine();
+        Console.Write(grade);
+        int gradeNum = int.Parse(grade);
+        string gradeLetter;
+        bool pass;
+        if (gradeNum >= 90)
+        {
+            gradeLetter = "A";
+            pass = true;
+        }
+        else if (gradeNum >= 80)
+        {
+            gradeLetter = "B";
+            pass = true;
+        }
+        else if (gradeNum >= 70)
+        {
+            gradeLetter = "C";
+            pass = true;
+        }
+        else if (gradeNum >= 60)
+        {
+            gradeLetter = "D";
+            pass = false;
+        }
+        else
+        {
+            gradeLetter = "F";
+            pass = false;
+        }
+        int lastDigit = gradeNum % 10;
+        string plusMinus;
+        if (lastDigit < 3)
+        {
+            plusMinus = "-";
+        }
+        else if (lastDigit  >= 7)
+        {
+            plusMinus = "+";
+        }
+        else
+        {
+            plusMinus = "N/A";
+        }
+        if (plusMinus != "N/A" && (gradeLetter != "A" && gradeLetter != "F"))
+        {
+            gradeLetter = gradeLetter + plusMinus;
+        }
+        else if (gradeLetter == "A" && plusMinus == "-")
+        {
+            gradeLetter = gradeLetter + plusMinus;
+        }
+        Console.WriteLine($"You acheived a {gradeLetter} letter grade");
+        if (pass == true)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Congratulations! You passed the class");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Sorry! You failed the course.");
+        }
+        
+    }   
 }
