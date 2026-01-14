953da6c9112255e52c244d027ea192dde63da3d1
Brian Walsh <wal21017@byui.edu>
2026-01-14 15:29:53 -0700

    Program 4 Complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,25 @@
 using System;
-
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers =  new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        int number;
+        do 
+        {   
+            Console.Write("Enter number: ");
+            string num = Console.ReadLine();
+            number = int.Parse(num);
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while(number != 0);
+        Console.WriteLine($"The sum is {numbers.Sum()}");
+        Console.WriteLine($"The average is {numbers.Average()}");
+        Console.WriteLine($"The largest number is {numbers.Max()}");
     }
 }
