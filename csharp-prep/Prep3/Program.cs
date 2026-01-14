7be6d1c6fe9ba2d3be526f91964667313e871aa0
Brian Walsh <wal21017@byui.edu>
2026-01-14 15:14:00 -0700

    Completed Prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int m_n = randomGenerator.Next(1,101);
+        int g_n;
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            g_n = int.Parse(guess);
+            if (g_n == m_n)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (g_n > m_n)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (g_n < m_n)
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (g_n != m_n);
+
+
     }
 }
