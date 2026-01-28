adee5d5cedc1b64473b966b851d799282ba69a0b
Brian Walsh <wal21017@byui.edu>
2026-01-27 17:23:46 -0700

    Finished Learning activity two, possibly some errors but it appears to be working fine

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        
+        Job job1 = new Job();
+        job1._jobTitle = "Campus Security Dispatcher";
+        job1._company = "BYU-Idaho";
+        job1._startYear = 2024;
+        job1._endYear = 2026;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Cashier and Coach";
+        job2._company = "Gravity Factory";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+        
+
+        Resume new_resume = new Resume();
+        new_resume._name = "Brian Walsh";
+        new_resume._jobs.Add(job1);
+        new_resume._jobs.Add(job2);
+        new_resume.Display();
     }
 }
